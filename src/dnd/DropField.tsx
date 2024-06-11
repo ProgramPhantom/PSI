67746214629d549ed1cf5c84f8c4fdd456702cd1
ProgramPhantom <henryvarley@outlook.com>
2024-06-11 14:58:36 +0100

    Insert end implemented

--- src/dnd/DropField.tsx
+++ src/dnd/DropField.tsx
@@ -4,6 +4,7 @@ import { CSSProperties, useEffect, useState } from "react";
 import SequenceHandler from "../vanilla/sequenceHandler";
 import InsertArea, { AddSpec } from "./InsertArea";
 import { Orientation } from "../vanilla/positional";
+import { Dimensions } from "../vanilla/spacial";
 
 class SequenceDropInterpreter {
     public handler: SequenceHandler;
@@ -68,7 +69,31 @@ class SequenceDropInterpreter {
                         index: i, orientation: Orientation.bottom, channelName: name, insert: false}
                     this.insertAreas.push(newBlock);
                 } 
+
+
+                // insert end slithers
+                // Top slither
+                newSlither = {
+                    area: {x: column.getFar(Dimensions.X) - this.slitherWidth/2, 
+                        y: channel.y + (channel.annotationLayer ? channel.annotationLayer?.height : 0), 
+                        width: this.slitherWidth, 
+                        height: channel.maxTopProtrusion},
+                    index: i+1, orientation: Orientation.top, channelName: name, insert: true,
+                };
+                this.insertAreas.push(newSlither)
+
+                // bottom slither
+                newSlither = {
+                    area: {x: column.getFar(Dimensions.X) - this.slitherWidth/2, 
+                        y: channel.bar.y + channel.style.thickness + 1, 
+                        width: this.slitherWidth, 
+                        height: channel.maxBottomProtrusion + channel.padding[2] - 3},
+                    index: i+1, orientation: Orientation.bottom, channelName: name, insert: true
+                };
+                this.insertAreas.push(newSlither)
             })
+
+            
 
         })
     }
