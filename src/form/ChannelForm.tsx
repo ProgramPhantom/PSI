da09dc9d474cb60e5e50f73501993afe85dc94c2
ProgramPhantom <henryvarley@outlook.com>
2024-07-21 14:35:06 +0100

    After a week of a phantom bug, it magically starts working... Added channel form

--- src/form/ChannelForm.tsx
+++ src/form/ChannelForm.tsx
@@ -3,22 +3,25 @@ import * as ReactDOM from 'react-dom';
 import { Control, Controller, FieldValue, FieldValues, useForm, useWatch } from 'react-hook-form';
 import { ILabel } from '../vanilla/label';
 
-import { Button, ControlGroup, FormGroup, HTMLSelect, InputGroup, NumericInput, Section, Slider, Switch, Tab, Tabs, Tooltip } from "@blueprintjs/core";
+import { Button, ControlGroup, Divider, FormGroup, HTMLSelect, InputGroup, NumericInput, Section, Slider, Switch, Tab, Tabs, Tooltip } from "@blueprintjs/core";
 import LabelForm from './LabelForm';
 import SequenceHandler from '../vanilla/sequenceHandler';
 import { IChannel } from '../vanilla/channel';
 import { defaultChannel } from '../vanilla/default/data';
 
 
+
 function ChannelForm(props: {sequence: SequenceHandler}) {  
-    const { control, handleSubmit, formState: {isDirty, dirtyFields} } = useForm<IChannel>({
+    const { control, handleSubmit, formState: {isDirty, dirtyFields} } = useForm({
         defaultValues: {...(defaultChannel as any)},
         mode: "onChange"
     });
   
     function onSubmit(data: IChannel) {
-        alert(data);
+        // alert(data.identifier);
         props.sequence.channel(data.identifier, data);
+
+        props.sequence.draw();
     }
 
     return (
@@ -26,86 +29,85 @@ function ChannelForm(props: {sequence: SequenceHandler}) {
             <h3>New Channel</h3>
 
             <Tabs defaultSelectedTabId={"core"} animate={false} vertical={false}>
-                <Tab id={"core"} title="Core" panel={
+                <Tab id="core" title="Core" panel={
                     <ControlGroup vertical={true}>
-                        {/* Identifier */}
-                        <FormGroup fill={false}
-                            inline={true}
-                            label="Text"
-                            helperText="Channel identifier"
-                            labelFor="text-input">
-                            
+                        { /* Identifier */}
+                        <FormGroup
+                        fill={false}
+                        inline={true}
+                        label="Identifier"
+                        helperText="Name of channel"
+                        labelFor="text-input">
                             <Controller control={control} name="identifier" render={({field}) => (
                                 <InputGroup {...field} id="text" placeholder="h" small={true} />
                                 )}>
                             </Controller>
-
                         </FormGroup>
-
+                        
                         {/* Padding */}
-                        <Section style={{borderRadius: 0}}
+                        <Section
                             collapseProps={{defaultIsOpen: false}}
                             compact={true}
                             title={"Padding"}
-                            collapsible={true}
-                            >
+                            collapsible={true}>
                             <ControlGroup
-                                vertical={true}
-                                style={{gap: 10}}
-                                >
-                                <FormGroup
-                                fill={false}
-                                inline={true}
-                                label="Padding top"
-                                labelFor="text-input">
-                                <Controller control={control} name="padding.0" render={({field}) => (
-                                    <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
-                                </Controller>
-                                </FormGroup>
+                                vertical={true}>
 
                                 <FormGroup
-                                fill={false}
-                                inline={true}
-                                label="Padding right"
-                                labelFor="text-input">
-                                <Controller control={control} name="padding.1" render={({field}) => (
-                                    <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
-                                </Controller>
+                                    fill={false}
+                                    inline={true}
+                                    label="Padding top"
+                                    labelFor="text-input">
+                                    <Controller control={control} name="padding.0" render={({field: { onChange, onBlur, value, name, ref },}) => (
+                                        <Slider value={value} onChange={onChange} max={30} min={0} labelStepSize={10}></Slider>)}>
+                                    </Controller>
                                 </FormGroup>
-
+                    
                                 <FormGroup
-                                fill={false}
-                                inline={true}
-                                label="Padding bottom"
-                                labelFor="text-input">
-                                <Controller control={control} name="padding.2" render={({field}) => (
-                                    <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
-                                </Controller>
+                                    fill={false}
+                                    inline={true}
+                                    label="Padding right"
+                                    labelFor="text-input">
+                                    <Controller control={control} name="padding.1" render={({field}) => (
+                                        <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
+                                    </Controller>
                                 </FormGroup>
-
+                    
                                 <FormGroup
-                                fill={false}
-                                inline={true}
-                                label="Padding left"
-                                labelFor="text-input">
-                                <Controller control={control} name="padding.3" render={({field}) => (
-                                    <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
-                                </Controller>
+                                    fill={false}
+                                    inline={true}
+                                    label="Padding bottom"
+                                    labelFor="text-input">
+                                    <Controller control={control} name="padding.2" render={({field}) => (
+                                        <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
+                                    </Controller>
                                 </FormGroup>
+                    
+                                <FormGroup
+                                    fill={false}
+                                    inline={true}
+                                    label="Padding left"
+                                    labelFor="text-input">
+                                    <Controller control={control} name="padding.3" render={({field}) => (
+                                        <Slider {...field} max={30} min={0} labelStepSize={10}></Slider>)}>
+                                    </Controller>
+                                </FormGroup>
+                    
+                    
                             </ControlGroup>
                         </Section>
 
                         {/* Offset */}
                         <Section style={{borderRadius: 0}}
-                            collapseProps={{defaultIsOpen: false}}
-                            compact={true}
-                            title={"Offset"}
-                            collapsible={true}
+                        collapseProps={{defaultIsOpen: false}}
+                        compact={true}
+                        title={"Offset"}
+                        collapsible={true}
+                        >
+                        <ControlGroup
+                            vertical={true}
                             >
-                            <ControlGroup
-                                vertical={true}
-                                >
-                                <FormGroup
+                            <FormGroup
                                 fill={false}
                                 inline={true}
                                 label="Offset X"
@@ -113,24 +115,69 @@ function ChannelForm(props: {sequence: SequenceHandler}) {
                                 <Controller control={control} name="offset.0" render={({field}) => (
                                     <NumericInput {...field} onValueChange={field.onChange} min={-50} max={50} small={true}></NumericInput>)}>
                                 </Controller>
-                                </FormGroup>
+                            </FormGroup>
 
-                                <FormGroup
-                                fill={false}
-                                inline={true}
-                                label="Offset Y"
-                                labelFor="text-input">
-                                <Controller control={control} name="offset.1" render={({field}) => (
-                                    <NumericInput {...field} onValueChange={field.onChange} min={-50} max={50} small={true}></NumericInput>)}>
-                                </Controller>
-                                </FormGroup>
-                            </ControlGroup>
+                            <FormGroup
+                            fill={false}
+                            inline={true}
+                            label="Offset Y"
+                            labelFor="text-input">
+                            <Controller control={control} name="offset.1" render={({field}) => (
+                                <NumericInput {...field} onValueChange={field.onChange} min={-50} max={50} small={true}></NumericInput>)}>
+                            </Controller>
+                            </FormGroup>
+                        </ControlGroup>
                         </Section>
-                    </ControlGroup>
-                    }>
-                </Tab>
+                
+                        <Section 
+                            collapseProps={{defaultIsOpen: false}}
+                            compact={true}
+                            title={"Style"}
+                            collapsible={true}>
+                            <FormGroup
+                                label="Thickness"
+                                labelFor="text-input">
+                                <Controller control={control} name="style.thickness" render={({field}) => (
+                                    <NumericInput {...field} onValueChange={field.onChange} min={1} small={true} fill={true}></NumericInput>)}>
+                                </Controller>
+                            </FormGroup>
 
-                <Tab id="label" title="Label" panel={<LabelForm control={control} change={props.change}></LabelForm>}/>
+                            <Divider></Divider>
+            
+                            <FormGroup
+                                inline={true}
+                                label="Fill"
+                                labelFor="text-input">
+            
+                                <Controller control={control} name="style.fill" render={({field}) => (
+                                    <input type={"color"} {...field}></input>)}>
+                                </Controller>
+                            </FormGroup>
+            
+                            <FormGroup
+                                inline={true}
+                                label="Stroke"
+                                labelFor="text-input">
+            
+                                <Controller control={control} name="style.stroke" render={({field}) => (
+                                    <input type={"color"} {...field}></input>)}>
+                                </Controller>
+                            </FormGroup>
+            
+                            <FormGroup
+                                inline={true}
+                                label="Stroke Width"
+                                labelFor="text-input">
+                                <Controller control={control} name="style.strokeWidth" render={({field}) => (
+                                    <NumericInput {...field} onValueChange={field.onChange} min={1} small={true}></NumericInput>)}>
+                                </Controller>
+                            </FormGroup>
+                        </Section>
+            
+                    </ControlGroup>
+                }/>
+
+                <Tab id="label" title="Label" panel={<LabelForm control={control} change={() => {}}></LabelForm>}/>
 
             </Tabs>
 
@@ -139,8 +186,7 @@ function ChannelForm(props: {sequence: SequenceHandler}) {
 
             </input>
         </form>
-    );
+    )
 }
 
-    
 export default ChannelForm
