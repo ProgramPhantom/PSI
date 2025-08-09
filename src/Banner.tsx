5773e6be0b7a6339d2629ca71c1707a1e4e0a549
ProgramPhantom <henryvarley@outlook.com>
2025-08-09 16:04:52 +0100

    Updated title

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -1,4 +1,4 @@
-import { Alignment, Button, ButtonGroup, Navbar } from '@blueprintjs/core'
+import { Alignment, Button, ButtonGroup, Icon, Navbar } from '@blueprintjs/core'
 import React, { useEffect, useRef, useState } from 'react'
 import DraggableElement from './dnd/DraggableElement'
 
@@ -8,7 +8,8 @@ export default function Banner(props: {saveSVG: () => void, saveScript: () => vo
         <>
         <Navbar>
             <Navbar.Group align={Alignment.LEFT}>
-                <Navbar.Heading>Pulse Planner v0.1.3</Navbar.Heading>
+                <Icon icon="pulse" size={20} style={{marginRight: "10px"}}></Icon>
+                <Navbar.Heading>Pulse Planner v0.2.3 (BETA)</Navbar.Heading>
                 <Navbar.Divider />
 
                 <Button small={true} minimal={true} icon="cloud-download" text="Save SVG" onClick={props.saveSVG}/>
