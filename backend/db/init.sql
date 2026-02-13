2953f63f00d1d83690d45cff7ba824d42e7a4901
Conrad Robinson <conrad_str@outlook.com>
2026-02-13 14:32:44 +0000

    database schema generated from init.sql on docker compose

---
+++ backend/db/init.sql
@@ -0,0 +1,30 @@
+create table if not exists users
+(
+    gsub      varchar not null
+        constraint pkey
+            primary key,
+    email     varchar not null,
+    firstname varchar not null,
+    surname   varchar not null,
+    picture   varchar
+);
+
+alter table users
+    owner to postgres;
+
+create table if not exists diagrams
+(
+    diagram_id    uuid      not null
+        constraint diagrams_pk
+            primary key,
+    data          jsonb     not null,
+    date_created  timestamp not null,
+    date_modified timestamp not null,
+    owner         varchar   not null
+        constraint diagrams_users_gsub_fk
+            references users
+);
+
+alter table diagrams
+    owner to postgres;
+
