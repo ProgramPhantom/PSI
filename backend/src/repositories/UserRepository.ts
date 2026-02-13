e83142a85a1d92de3aa422744d6f0fddc7e36041
Conrad Robinson <conrad_str@outlook.com>
2026-02-13 23:17:23 +0000

    User Repository

--- backend/src/repositories/UserRepository.ts
+++ backend/src/repositories/UserRepository.ts
@@ -1,3 +1,15 @@
+import { db } from '../db/database.js';
+import type { Insertable } from 'kysely'
+import type { Users } from '../db/db.js';
+
 export const UserRepository = {
-    
+    async createUser(newUser: Insertable<Users>) {
+        return await db.insertInto('users').values(newUser).executeTakeFirst()
+    },
+    async getUserById(id: string) {
+        return await db.selectFrom('users').selectAll().where('gsub', '=', id).executeTakeFirst()
+    },
+    async deleteUserById(id: string) {
+        return await db.deleteFrom('users').where('gsub', '=', id).executeTakeFirst();
+    }
 }
