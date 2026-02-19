7d1a235d660cc09a33b7a0a631a9798a4822f2d7
ProgramPhantom <henryvarley@outlook.com>
2026-02-19 20:01:10 +0000

    Implemented API call in Diagrams dialog

--- frontend/src/features/banner/DiagramsDialog.tsx
+++ frontend/src/features/banner/DiagramsDialog.tsx
@@ -1,9 +1,12 @@
-import { Button, Dialog, DialogBody, DialogFooter, HTMLTable } from "@blueprintjs/core";
-import { IDiagram } from "../../logic/hasComponents/diagram";
+import { Button, Dialog, DialogBody, DialogFooter, HTMLTable, NonIdealState, Spinner } from "@blueprintjs/core";
+import Diagram, { IDiagram } from "../../logic/hasComponents/diagram";
 import { ISequenceAligner } from "../../logic/hasComponents/sequenceAligner";
 import ENGINE from "../../logic/engine";
 import { DEFAULT_DIAGRAM } from "../../logic/default/defaultDiagram";
 import { BLANK_DIAGRAM } from "../../logic/default/blankDiagram";
+import { getUserDiagrams } from "../../logic/api";
+import { useEffect, useState } from "react";
+import { More } from "@blueprintjs/icons";
 
 export interface IDiagramsDialogProps {
     isOpen: boolean;
@@ -33,11 +36,35 @@ const DUMMY_DIAGRAMS: IDiagramEntry[] = [
     blank
 ];
 
+interface DiagramDTO {
+    name?: string | undefined,
+    diagram_id?: string | undefined
+}
+
 export function DiagramsDialog(props: IDiagramsDialogProps) {
-    const handleSelect = (entry: IDiagramEntry) => {
-        ENGINE.handler.constructDiagram(entry.diagram);
+    const handleSelect = (entry: DiagramDTO) => {
+        //ENGINE.handler.constructDiagram(entry.diagram);
         props.onClose();
     };
+
+    const [diagramList, setDiagramList] = useState<DiagramDTO[]>([])
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        console.log("fetching data")
+        getUserDiagrams().then((response) => {
+        if (response.error) {
+            setIsLoaded(false)
+            return
+        }
+        let data: DiagramDTO[] = response.data?.diagrams?.filter(d => d !== undefined) ?? []
+        setDiagramList(data);
+        console.log(`Set diagram ids ${data}`)
+        setIsLoaded(true)
+    }
+    );
+    }, [props.isOpen])
+
 
     return (
         <Dialog
@@ -47,7 +74,7 @@ export function DiagramsDialog(props: IDiagramsDialogProps) {
             style={{ width: "700px", height: "600px" }}
         >
             <DialogBody style={{ padding: "8px", overflowY: "auto" }}>
-                <HTMLTable bordered striped interactive style={{ width: "100%" }}>
+                {isLoaded && <HTMLTable bordered striped interactive style={{ width: "100%" }}>
                     <thead style={{ position: "sticky", top: 0, zIndex: 1, background: "var(--pt-app-background-color, #fff)" }}>
                         <tr>
                             <th>Name</th>
@@ -56,19 +83,31 @@ export function DiagramsDialog(props: IDiagramsDialogProps) {
                         </tr>
                     </thead>
                     <tbody>
-                        {DUMMY_DIAGRAMS.map((entry) => (
+                        {diagramList.map((entry, i) => (
                             <tr
-                                key={`${entry.diagram.id + Math.random().toString()}`}
+                                key={`${entry.diagram_id + Math.random().toString()}`}
                                 onClick={() => handleSelect(entry)}
                                 style={{ cursor: "pointer" }}
                             >
-                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>{entry.diagram.ref}</td>
-                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>{entry.dateModified}</td>
-                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>{entry.owner}</td>
+                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>{entry.name}</td>
+                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>today</td>
+                                <td style={{ paddingTop: 4, paddingBottom: 4 }}>Owner</td>
                             </tr>
                         ))}
                     </tbody>
-                </HTMLTable>
+                </HTMLTable>}
+
+                <div>
+                    {diagramList.length == 0 && 
+                <NonIdealState description="No diagrams saved" icon={<More></More>}></NonIdealState>}
+
+                {!isLoaded && diagramList.length != 0 && <>
+                    <div style={{
+                    }}>
+                        <NonIdealState description="Loading diagrams" icon={<Spinner></Spinner>}></NonIdealState>
+                    </div>
+                </>}
+                </div>
             </DialogBody>
             <DialogFooter
                 actions={
