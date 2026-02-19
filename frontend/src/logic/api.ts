cb4e5779bea4a989ae99ca8a571a52ac7bed7b33
Conrad Robinson <conrad_str@outlook.com>
2026-02-19 16:51:04 +0000

    setup api routes function

---
+++ frontend/src/logic/api.ts
@@ -0,0 +1,37 @@
+
+
+import {CredentialResponse} from "@react-oauth/google";
+import { paths } from "../types/apiSchema";
+import createClient from 'openapi-fetch'
+import { IDiagram } from "./hasComponents/diagram";
+
+
+
+const client = createClient<paths>({ baseUrl: "/api/" });
+
+export const getUserDiagrams = async () => {
+    return await client.GET('/users/diagrams')
+}
+export const getDateModified = async (diagramId: string) => {
+    return await client.GET('/diagrams/{diagramId}/modified', { params: {path: {diagramId: diagramId}}})
+}
+
+export const getDiagram = async (diagramId: string) => {
+    return await client.GET('/diagrams/{diagramId}', {params: {path: {diagramId: diagramId}}})
+}
+
+export const loginUser = async (googleData: CredentialResponse) => {
+    return await client.POST('/users/login', {body: googleData})
+}
+
+export const createDiagram = async (name: string) => {
+    return await client.POST('/diagrams', {body: {name: name}})
+}
+export const saveDiagram = async (diagramId: string, diagram: IDiagram) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return await client.PUT('/diagrams/{diagramId}', {params: {path: {diagramId: diagramId}}, body: diagram as any})
+}
+
+export const deleteDiagram = async (diagramId: string) => {
+    return await client.DELETE('/diagrams/{diagramId}', {params: {path: {diagramId: diagramId}}})
+}
