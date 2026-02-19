50886d2e168f82ee58e3a9c072db15d4872c819b
Conrad Robinson <conrad_str@outlook.com>
2026-02-19 13:39:26 +0000

    made frontend connect correctly to server

--- frontend/src/logic/login.ts
+++ frontend/src/logic/login.ts
@@ -3,7 +3,14 @@ import {CredentialResponse} from "@react-oauth/google";
 export const login = async (body: CredentialResponse) => {
 	const url = "/api/users/login";
 	try {
-		const response = await fetch(url, {method: "POST", body: JSON.stringify(body)});
+		console.log(JSON.stringify(body));
+		const response = await fetch(url, {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
 		if (!response.ok) {
 			throw new Error(
 				`Response status: ${response.status} - Response Body: ${response.body}`
