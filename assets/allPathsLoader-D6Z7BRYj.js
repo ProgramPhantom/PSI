ec7cce0c957f261370597a690655b158c9ad1999
ProgramPhantom <henryvarley@outlook.com>
2026-06-16 22:08:34 +0100

    Updates

---
+++ assets/allPathsLoader-D6Z7BRYj.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-D1s9KUyY.js","assets/index-DLOviMB1.js","assets/index-B-fsa5Ru.js","assets/index-DO1CzWxE.js","assets/index-SlmjeILh.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-DO1CzWxE.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-D1s9KUyY.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+//# sourceMappingURL=allPathsLoader-D6Z7BRYj.js.map
--- frontend/src/logic/default/simplePulse/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Pulse90 from "./90pulse";
-import * as Pulse180 from "./180pulse";
-
-export const simplePulses = {
-	"90-pulse": Pulse90,
-	"180-pulse": Pulse180
-};
